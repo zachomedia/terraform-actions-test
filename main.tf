db43707d892e2d1cedb84bb53b81f57b5bada8d7
Zachary Seguin <zachary@zacharyseguin.ca>
2020-03-29 17:03:26 -0400

    fix(azure): Generate random name

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-resource "random_string" "random" {
+resource "random_string" "name" {
   length  = 16
   special = false
   upper   = false
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name     = "${random_string.random.result}-rg"
+  name     = "${random_string.name.result}-rg"
   location = "Canada Central"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = random_string.random.result
+  name                     = random_string.name.result
   location                 = "Canada Central"
   resource_group_name      = ""
   account_tier             = "Standard"
