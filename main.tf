9ae7aa1dcfbb2171682177df4d0a5510a2d3563c
Zachary Seguin <zachary@zacharyseguin.ca>
2020-03-29 17:38:21 -0400

    chore(cleanup): Cleanup create test resources

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-resource "random_string" "name" {
-  length  = 16
-  special = false
-  upper   = false
-}
+# resource "random_string" "name" {
+#   length  = 16
+#   special = false
+#   upper   = false
+# }
 
-resource "azurerm_resource_group" "resource_group" {
-  name     = "${random_string.name.result}-rg"
-  location = "Canada Central"
-}
+# resource "azurerm_resource_group" "resource_group" {
+#   name     = "${random_string.name.result}-rg"
+#   location = "Canada Central"
+# }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = random_string.name.result
-  location                 = "Canada Central"
-  resource_group_name      = azurerm_resource_group.resource_group.name
-  account_tier             = "Standard"
-  account_kind             = "BlobStorage"
-  account_replication_type = "LRS"
-  # enable_blob_encryption = "True"
-  # enable_file_encryption = "True"
-  access_tier = "Hot"
-}
+# resource "azurerm_storage_account" "storage" {
+#   name                     = random_string.name.result
+#   location                 = "Canada Central"
+#   resource_group_name      = azurerm_resource_group.resource_group.name
+#   account_tier             = "Standard"
+#   account_kind             = "BlobStorage"
+#   account_replication_type = "LRS"
+#   # enable_blob_encryption = "True"
+#   # enable_file_encryption = "True"
+#   access_tier = "Hot"
+# }
