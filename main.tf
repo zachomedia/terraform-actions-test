d0ca8c66a6117d291098a386302c1e85f03ba378
Zachary Seguin <zachary@zacharyseguin.ca>
2020-03-29 16:54:37 -0400

    feat(azure): Add resource group and storage account

--- main.tf
+++ main.tf
@@ -1,5 +1,21 @@
 resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
+  length  = 16
+  special = false
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "${random_string.random.result}-rg"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = random_string.random.result
+  location                 = "Canada Central"
+  resource_group_name      = ""
+  account_tier             = "Standard"
+  account_kind             = "BlobStorage"
+  account_replication_type = "LRS"
+  # enable_blob_encryption = "True"
+  # enable_file_encryption = "True"
+  access_tier = "Hot"
 }
