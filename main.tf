070c8755d59958b8b4bdf685d78a1bfea681de5a
Zachary Seguin <zachary@zacharyseguin.ca>
2020-03-29 17:09:06 -0400

    fix(azure): Add resource group name to storage account

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "resource_group" {
 resource "azurerm_storage_account" "storage" {
   name                     = random_string.name.result
   location                 = "Canada Central"
-  resource_group_name      = ""
+  resource_group_name      = azurerm_resource_group.resource_group.name
   account_tier             = "Standard"
   account_kind             = "BlobStorage"
   account_replication_type = "LRS"
